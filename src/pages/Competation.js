2cc1935339d97e5d6f0f97b9f68f4c15ece495d6
hassamkazmi <muhammadaslambhai@gmail.com>
2022-09-16 19:12:15 +0500

    hassam1

---
+++ src/pages/Competation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Competation = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Competation
---
+++ src/pages/Races.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Races = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Races
