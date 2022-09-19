05414c03747c8bf2c1afe57b357b2e440e4a13fe
hassamkazmi <hassamsyed7@gmail.com>
2022-09-19 12:46:27 +0500

    add

--- src/Components/Common/Auth.js
+++ src/Components/Common/Auth.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Auth = () => {
   return (
     <div>
-      
+      <h1>Auth Data</h1>
     </div>
   )
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../Components/Common/Header';
+import Sidebar from '../Components/Common/Sidebar';
+import '../Components/CSS/home.css'
 
 const Dashboard = () => {
   return (
     <div>
-      
+      <Header />
+      <Sidebar />
     </div>
   )
 }
