9264c595e1343ed61717095c9a9213bca0b796e5
Syed Hassam Kazmi <59208947+Hassamkazmi@users.noreply.github.com>
2022-10-18 01:32:41 +0500

    new

--- src/pages/PostTable/RaceForm/RaceForm.js
+++ src/pages/PostTable/RaceForm/RaceForm.js
@@ -21,6 +21,7 @@ const RaceForm = () => {
   const { data: horse } = useSelector((state) => state.horse);
   const history = useNavigate();
 
+  const [data, setData] = useState(true);
 
     let horseoptions = horse.map(function (item) {
     return {
@@ -41,7 +42,19 @@ const RaceForm = () => {
   const [RaceStatus, setRaceStatus] = useState("");
   const [RaceCourse, setRaceCourse] = useState("");
   const [Horses, setHorses] = useState("");
+  const [selectedOption1, setSelectedOption1] = useState('');
+  const [selectedOption2, setSelectedOption2] = useState('');
+  const [selectedOption3, setSelectedOption3] = useState('');
+  const [selectedOption4, setSelectedOption4] = useState('');
+  const [selectedOption5, setSelectedOption5] = useState('');
+  const [selectedOption6, setSelectedOption6] = useState('');
 
+  // const addTodo = id => {
+  //   const newTodos = [...todos, { id }];
+  //   setTodos(newTodos);
+
+  // };
+  
   useEffect(() => {
     dispatch(fetchHorse());
     dispatch(fetchracecourse());
@@ -59,7 +72,12 @@ const RaceForm = () => {
       formData.append("Weather", Weather);
       formData.append("RaceStatus", RaceStatus);
       formData.append("RaceCourse", RaceCourse);
-      formData.append("Horses", Horses);
+      formData.append("Horses",( selectedOption1.id))
+      formData.append("Horses", selectedOption2.id)
+      formData.append("Horses", selectedOption3.id)
+      formData.append("Horses", selectedOption4.id)
+      formData.append("Horses", selectedOption5.id)
+      formData.append("Horses", selectedOption6.id)
       dispatch(add(formData));
       history("/races");
     } catch (error) {
@@ -67,10 +85,14 @@ const RaceForm = () => {
     }
   };
 
+  function handleRace(){
+    setData(false)
+  }
   return (
     <>
       <Header />
-      <div className="page">
+      {
+        data ? <div className="page">
         <Sidebar />
         <div className="rightsidedata">
           <div
@@ -80,7 +102,7 @@ const RaceForm = () => {
           >
             <div className="Headers">Add Race</div>
             <div className="form">
-              <form onSubmit={submit}>
+              <form>
                 <div className="row ">
                   <div className="col-sm">
                     <input
@@ -180,7 +202,7 @@ const RaceForm = () => {
                     </select>
                   </div>
                 </div>
-                <div className="row ">
+                {/* <div className="row ">
                   <div className="col-sm">
                     <Select placeholder={<div>Type to Add Horses</div>}
                         defaultValue={Horses}
@@ -203,7 +225,7 @@ const RaceForm = () => {
                         isSearchable={true}
                       />
                   </div>
-                </div>
+                </div> */}
                 <div className="row ">
                   <div className="col-sm">
                     <select
@@ -245,9 +267,8 @@ const RaceForm = () => {
                   <button className="updateButton">updated</button>
 
               
-                  <button className="SubmitButton" type="submit"
-                  
-                  // onClick={() => navigate('/addhorse')}
+                  <button className="SubmitButton"
+                  onClick={handleRace}
                   >
                     Add Horse
                   </button>
@@ -257,7 +278,137 @@ const RaceForm = () => {
             </div>
           </div>
         </div>
+      </div> :
+      <div className="page">
+      <Sidebar />
+      <div className="rightsidedata">
+        <div
+          style={{
+            marginTop: "30px",
+          }}
+        >
+          <div className="Header ">
+            <h4>Add Horse</h4>
+          </div>
+          <div className="myselecthorse">
+              <div className="myselecthorsedata">
+                  <span>Gate #</span>
+                  <span>Horse Name</span>
+                  <span>Jockey Name</span>
+              </div>
+          </div>
+          <div className="myselectdata">
+              <div className="myselectiondata">
+                  <span>#1</span>
+                  <span>
+                  <Select
+                      defaultValue={selectedOption1}
+                      onChange={setSelectedOption1}
+                      options={horseoptions}
+                      isClearable={true}
+                      isSearchable={true}
+                    />
+                  </span>
+                  <span>{selectedOption1 === '' ? (
+                      <p>N/A</p>
+                    ) : (
+                      <p>{selectedOption1.jockeyvalue[0]}</p>
+                    )}</span>
+              </div>
+              <div className="myselectiondata">
+                  <span>#2</span>
+                  <span>
+                  <Select
+                      defaultValue={selectedOption2}
+                      onChange={setSelectedOption2}
+                      options={horseoptions}
+                      isClearable={true}
+                      isSearchable={true}
+                    />
+                  </span>
+                  <span>{selectedOption2 === '' ? (
+                      <p>N/A</p>
+                    ) : (
+                      <p>{selectedOption2.jockeyvalue[0]}</p>
+                    )}</span>
+              </div>
+              <div className="myselectiondata">
+                  <span>#3</span>
+                  <span>
+                  <Select
+                      defaultValue={selectedOption3}
+                      onChange={setSelectedOption3}
+                      options={horseoptions}
+                      isClearable={true}
+                      isSearchable={true}
+                    />
+                  </span>
+                  <span>{selectedOption3 === '' ? (
+                      <p>N/A</p>
+                    ) : (
+                      <p>{selectedOption3.jockeyvalue[0]}</p>
+                    )}</span>
+              </div>
+              <div className="myselectiondata">
+                  <span>#4</span>
+                  <span>
+                  <Select
+                      defaultValue={selectedOption4}
+                      onChange={setSelectedOption4}
+                      options={horseoptions}
+                      isClearable={true}
+                      isSearchable={true}
+                    />
+                  </span>
+                  <span>{selectedOption4 === '' ? (
+                      <p>N/A</p>
+                    ) : (
+                      <p>{selectedOption4.jockeyvalue[0]}</p>
+                    )}</span>
+              </div>
+              <div className="myselectiondata">
+                  <span>#5</span>
+                  <span>
+                  <Select
+                      defaultValue={selectedOption5}
+                      onChange={setSelectedOption5}
+                      options={horseoptions}
+                      isClearable={true}
+                      isSearchable={true}
+                    />
+                  </span>
+                  <span>{selectedOption5 === '' ? (
+                      <p>N/A</p>
+                    ) : (
+                      <p>{selectedOption5.jockeyvalue[0]}</p>
+                    )}</span>
+              </div><div className="myselectiondata">
+                  <span>#6</span>
+                  <span>
+                  <Select
+                      defaultValue={selectedOption6}
+                      onChange={setSelectedOption6}
+                      options={horseoptions}
+                      isClearable={true}
+                      isSearchable={true}
+                    />
+                  </span>
+                  <span>{selectedOption6 === '' ? (
+                      <p>N/A</p>
+                    ) : (
+                      <p>{selectedOption6.jockeyvalue[0]}</p>
+                    )}</span>
+              </div>
+              <div className="sbmtbtn">
+                <button onClick={submit}>Save & Publish </button>
+              </div>
+          </div>
+          
+        </div>
       </div>
+    </div>
+      }
+      
     </>
   );
 };
