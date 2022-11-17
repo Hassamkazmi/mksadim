abe040e32c14cb6f32121b03f59e4f0c228b590c
hassamkazmi <hassamsyed7@gmail.com>
2022-11-17 15:20:43 +0500

    edit1

--- src/redux/getReducer/getSingleTrainer.js
+++ src/redux/getReducer/getSingleTrainer.js
@@ -8,32 +8,32 @@ export const STATUSES = Object.freeze({
     LOADING:'loading'
 })
 
-const getSingleJockey = createSlice({
-    name:'singlejockey',
+const getSingletrainer = createSlice({
+    name:'singletrainer',
     initialState:{
         data:[],
         status:STATUSES.IDLE
     },
      extraReducers:(builder) => {
         builder
-        .addCase(fetchSinglejockey.pending, (state, action) => {
+        .addCase(fetchSingletrainer.pending, (state, action) => {
             state.status = STATUSES.LOADING;
         })
-        .addCase(fetchSinglejockey.fulfilled, (state, action) => {
+        .addCase(fetchSingletrainer.fulfilled, (state, action) => {
             state.data = action.payload;
             state.status = STATUSES.IDLE;
         })
-        .addCase(fetchSinglejockey.rejected, (state, action) => {
+        .addCase(fetchSingletrainer.rejected, (state, action) => {
             state.status = STATUSES.ERROR;
         })
      },
 });
 
-export const {setjockey, setStatus} = getSingleJockey.actions;
-export default getSingleJockey.reducer;
-//https://mksbackend.herokuapp.com/api/v1/Jockeyget
-export const fetchSinglejockey = createAsyncThunk('getsinglejockey/fetch', async ({jockeyid}) => {
-    const res = await axios.get(`${window.env.API_URL}/singleJockey/${jockeyid}`);
+export const {settrainer, setStatus} = getSingletrainer.actions;
+export default getSingletrainer.reducer;
+//https://mksbackend.herokuapp.com/api/v1/trainerget
+export const fetchSingletrainer = createAsyncThunk('getsingletrainer/fetch', async ({trainerid}) => {
+    const res = await axios.get(`${window.env.API_URL}/singletrainer/${trainerid}`);
     const data = res.data;
     return data.data;
 })
